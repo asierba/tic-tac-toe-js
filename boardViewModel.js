0560b8fd1b8dd054aaff9892e1b01ede01836152
Asier Barrenetxea <asierba@gmail.com>
2014-04-08 21:46:14 +0100

    Check for already taken square

--- boardViewModel.js
+++ boardViewModel.js
@@ -1,27 +1,64 @@
 function BoardViewModel() {
-  'use strict';
-  var self = this;  
-
-  function initialiseSquares() {
-    for(var i=0; i < 9; i++)  {
-      self.squares.push(ko.observable(''));
-    }
-  }
-
-  function isLastSquare(postion) {
-    return postion === self.squares.length - 1;
-  }
-
-  self.squares = [];
-  self.select = function (userPostion) {
-    self.squares[userPostion]('X');
+    'use strict';
+    var self = this,
+        emptyMark = '',
+        userMark = 'X',
+        cpuMark = 'O';
     
-    var cpuPositon = userPostion+1;
-    if(isLastSquare(userPostion)) {
-      cpuPositon = userPostion-1;
-    } 
-    self.squares[cpuPositon]('O');
-  };
+    self.squares = [];
 
-  initialiseSquares();
+    function isFree(position) {
+        return self.squares[position]() === emptyMark;
+    }    
+
+    function nextAvailableSquare(currentPostion) {
+        if (!isFree(currentPostion)) {
+            currentPostion += 1;
+            return nextAvailableSquare(currentPostion);
+        }
+        return currentPostion;
+    }
+
+    function isLastSquare(position) {
+        return position === self.squares.length - 1;
+    }
+
+    function allTaken() {
+        for (var i=0; i < 9; i++)  {
+            if(isFree(i)) {
+                return false;               
+            }
+        } 
+        return true;
+    }
+
+    function cpuMove(userPostion) {
+        if(allTaken()) {
+            return;
+        }
+        var cpuPositon = userPostion + 1;
+        if (isLastSquare(userPostion)) {
+            cpuPositon = 0;
+        }
+        cpuPositon = nextAvailableSquare(cpuPositon);
+
+        self.squares[cpuPositon](cpuMark);
+    }
+
+    self.move = function (position) {
+        if (!isFree(position)) {
+            return;
+        }
+        self.squares[position](userMark);
+
+        cpuMove(position);
+    };
+
+    function initialiseSquares() {
+        for (var i=0; i < 9; i++)  {
+            self.squares.push(ko.observable(emptyMark));
+        }
+    }
+
+    initialiseSquares();
 }
--- specs/boardViewModel.spec.js
+++ specs/boardViewModel.spec.js
@@ -1,43 +1,159 @@
 describe('when loading the board', function() {
-  var board;
-  
-  beforeEach(function() { 
-    board = new BoardViewModel();
-  });  
-
-  it('all squares are free', function() {
-    for (var i = 0; i < board.squares.length ; i++) {
-      expect(board.squares[i]()).toBe('');
-    };
-  });
-
-  describe('when user clicks in a square', function() {
+    var board;
+    
     beforeEach(function() { 
-      board.select(3);
+        board = new BoardViewModel();
     });  
 
-    it('should be marked with an X', function() {
-      expect(board.squares[3]()).toBe('X');
+    it('all squares are free', function() {
+        for (var i = 0; i < board.squares.length ; i++) {
+          expect(board.squares[i]()).toBe('');
+        };
     });
 
-    it('following square should be marked with an O', function() {
-      expect(board.squares[4]()).toBe('O');
-    });
-  }); 
+    describe('and user clicks in a square', function() {
+        beforeEach(function() { 
+            board.move(3);
+        });  
 
-   describe('when user clicks in the last square of the board', function() {
-    var indexLast = 8;
-    beforeEach(function() { 
-      board.select(indexLast);
-    });  
+        it('should be marked with an X', function() {
+            expect(board.squares[3]()).toBe('X');
+        });
 
-    it('should be marked with an X', function() {
-      expect(board.squares[indexLast]()).toBe('X');
-    });
+        it('following square should be marked with an O', function() {
+            expect(board.squares[4]()).toBe('O');
+        });
+    }); 
 
-    it('previous square should be marked with an O', function() {
-      expect(board.squares[indexLast-1]()).toBe('O');
-    });
-  }); 
+    describe('and user clicks in the last square of the board', function() {
+        var indexLast = 8;
+        beforeEach(function() { 
+            board.move(indexLast);
+        });  
+
+        it('should be marked with an X', function() {
+            expect(board.squares[indexLast]()).toBe('X');
+        });
+
+        it('first square should be marked with an O', function() {
+            expect(board.squares[0]()).toBe('O');
+        });
+    }); 
 });
 
+describe('when user clicks in taken square', function() {
+    var board;
+    
+    beforeEach(function() { 
+        board = new BoardViewModel();
+        board.squares[4]('O');
+        board.squares[5]('X');
+     
+        board.move(4);
+    });  
+
+    it('should not take that square', function() {
+        expect(board.squares[4]()).not.toBe('X');
+    });
+    
+    it('cpu should not make a move', function() {
+        expect(board.squares[5]()).not.toBe('O');
+    });
+});
+
+describe('when user clicks in square next to taken square', function() {
+    var board;
+    
+    beforeEach(function() { 
+        board = new BoardViewModel();
+        board.squares[4]('X');
+        board.squares[8]('O');
+     
+        board.move(3);
+    });  
+
+    it('should take that square', function() {
+        expect(board.squares[3]()).toBe('X');
+    });
+    
+    it('cpu should not take already taken square', function() {
+        expect(board.squares[4]()).not.toBe('O');
+    });
+
+    it('cpu should take the next available', function() {
+        expect(board.squares[5]()).toBe('O');
+    });
+});
+
+describe('when user clicks in square next to two taken squares', function() {
+    var board;
+    
+    beforeEach(function() { 
+        board = new BoardViewModel();
+        board.squares[4]('X');
+        board.squares[5]('O');
+     
+        board.move(3);
+    });  
+
+    it('should take that square', function() {
+        expect(board.squares[3]()).toBe('X');
+    });
+    
+    it('cpu should not take already taken square', function() {
+        expect(board.squares[4]()).not.toBe('O');
+    });
+
+    it('cpu should take the next available', function() {
+        expect(board.squares[6]()).toBe('O');
+    });
+});
+
+describe('when user clicks in square next to four taken squares', function() {
+    var board;
+    
+    beforeEach(function() { 
+        board = new BoardViewModel();
+        board.squares[4]('X');
+        board.squares[5]('O');
+        board.squares[6]('X');
+        board.squares[7]('O');
+     
+        board.move(3);
+    });  
+
+    it('should take that square', function() {
+        expect(board.squares[3]()).toBe('X');
+    });
+    
+    it('cpu should not take already taken square', function() {
+        expect(board.squares[4]()).not.toBe('O');
+    });
+
+    it('cpu should take the next available', function() {
+        expect(board.squares[8]()).toBe('O');
+    });
+});
+
+
+describe('when no more free spaces to be taken by cpu', function() {
+    var board;
+    
+    beforeEach(function() { 
+        board = new BoardViewModel();
+        board.squares[0]('X');
+        board.squares[1]('O');
+        board.squares[2]('X');
+        board.squares[3]('O');
+        board.squares[4]('X');
+        board.squares[5]('O');
+        board.squares[6]('X');
+        board.squares[7]('O');        
+     
+        board.move(8);
+    });  
+
+    it('game should end', function() {
+        // empty verification to check in the browser that we dont get error
+    });
+});
