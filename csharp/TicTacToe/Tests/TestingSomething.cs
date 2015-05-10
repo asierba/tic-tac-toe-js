555d79a9026aa0f22fe6b12fd852ee0eb4a77616
asierba <asierba@gmail.com>
2015-05-11 00:00:19 +0100

    Game is over when player takes a column

--- csharp/TicTacToe/Tests/TestingSomething.cs
+++ csharp/TicTacToe/Tests/TestingSomething.cs
@@ -16,7 +16,9 @@ namespace Tests
         [Test]
         public void GameIsNotOverWhenAllFieldsNotTaken()
         {
-            var fields = new[] {'X', 'O', 'O', 'X', ' ', 'O', 'X', 'O', 'O'};
+            var fields = new[] {'X', 'O', 'O', 
+                                'X', ' ', 'O', 
+                                'O', 'X', 'X'};
             Assert.That(false, Is.EqualTo(GameIsOver(fields)));
         }
 
@@ -39,19 +41,55 @@ namespace Tests
         [TestCase(new[] { ' ', ' ', ' ', 
                           ' ', ' ', ' ', 
                           'X', 'X', 'X' })]
+        //[TestCase(new[] {'X', 'O', 'O', 
+        //                'X', ' ', 'O', 
+        //                'O', 'O', 'X'})] //TODO special case - needs revisit
+
         public void GameIsOverWhenAllFieldsInARowAreTakenByAPlayer(char[] fields)
+        {
+            Assert.That(true, Is.EqualTo(GameIsOver(fields)));
+        }
+
+        [TestCase(new[] { 'X', ' ', ' ', 
+                          'X', ' ', ' ', 
+                          'X', ' ', ' ' })]
+        [TestCase(new[] { ' ', ' ', 'X', 
+                          ' ', ' ', 'X', 
+                          ' ', ' ', 'X' })]
+        public void GameIsOverWhenAllFieldsInAColumnAreTakenByAPlayer(char[] fields)
         {
             Assert.That(true, Is.EqualTo(GameIsOver(fields)));
         }
 
         private static bool GameIsOver(IReadOnlyList<char> fields)
         {
+            return PlayerTookARow(fields) || 
+                PlayerTookAColumn(fields) || 
+                AllTaken(fields);
+        }
+
+        private static bool PlayerTookARow(IReadOnlyList<char> fields)
+        {
             for (var i = 0; i < fields.Count() - 2; i++)
             {
-                if (fields[i] != ' ' && fields[i] == fields[i+1] && fields[i] == fields[i+2])
+                if (fields[i] != ' ' && fields[i] == fields[i + 1] && fields[i] == fields[i + 2])
                     return true;
             }
+            return false;
+        }
 
+        private static bool PlayerTookAColumn(IReadOnlyList<char> fields)
+        {
+            for (var i = 0; i < 3; i++)
+            {
+                if (fields[i] != ' ' && fields[i] == fields[i + 3] && fields[i] == fields[i + 6])
+                    return true;
+            }
+            return false;
+        }
+
+        private static bool AllTaken(IReadOnlyList<char> fields)
+        {
             return !fields.Contains(' ');
         }
 
